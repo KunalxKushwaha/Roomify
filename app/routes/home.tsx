4357d9baf9c51f2adeb8e7583033d06e4f0ec90b
KunalxKushwaha <kushwahakunal733@gmail.com>
2026-04-17 23:34:00 +0530

    Readme updated

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,8 +4,8 @@ import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Roomify | Kunal Kushwaha" },
+    { name: "description", content: "Welcome to Roomify" },
   ];
 }
 
@@ -15,14 +15,6 @@ export default function Home() {
       <Navbar/>
       <h1 className="text-3xl text-indigo-700 font-extrabold">Home</h1>
       
-      {/* Button component demo */}
-      <div className="p-8 flex flex-col gap-4 max-w-md mx-auto mt-12">
-        <h2 className="text-xl font-bold">Button Demo:</h2>
-        <Button>Default (primary md)</Button>
-        <Button variant="secondary" size="sm">Secondary Small</Button>
-        <Button variant="ghost" size="lg" fullWidth> Ghost Large Full</Button>
-        <Button variant="outline" className="hover:scale-105">Outline Custom</Button>
-      </div>
     </div>
   )
 }
